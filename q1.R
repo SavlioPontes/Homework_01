95e27d6af2d0d2b32453df21d4af14e1c28c490b
SavlioPontes <savlio.cpontes@gmail.com>
2025-10-05 00:46:47 -0300

    Question1_Item1

---
+++ q1.R
@@ -0,0 +1,39 @@
+emissions <- scan(); #Allows you to read all the grouped data
+15.8 22.7 26.8 19.1 18.5 14.4 8.3 25.9 26.4 9.8 21.9 10.5
+17.3 6.2 18.0 22.9 24.6 19.4 12.3 15.9 20.1 17.0 22.3 27.5
+23.9 17.5 11.0 20.4 16.2 20.8 20.9 21.4 18.0 24.3 11.8 17.9
+18.7 12.8 15.5 19.2 13.9 28.6 19.4 21.6 13.5 24.6 20.0 24.1
+9.0 17.6 25.7 20.1 13.2 23.7 10.7 19.0 14.5 18.1 31.8 28.5
+22.7 15.2 23.0 29.6 11.2 14.7 20.5 26.6 13.3 18.1 24.8 26.1
+7.7 22.5 19.3 19.4 16.7 16.9 23.5 18.4
+
+
+#Central tendency measures:
+mean_emissions <- mean(emissions); #Calculates the mean of emissions
+median_emissions <- median(emissions); #Calculates the median of emissions
+#Function for mode:
+mode <- function(x){
+  ux <- unique(x); #Vector containing each unique element of the original vector
+  count <- tabulate(match(x,ux)); #Counts how many times each element occurs
+  if(all(count == 1)){
+    return(NA); #If no elements are repeated,there is no mode
+  }else{
+    return(ux[which.max(count)]); #If there are repetitions,get the most frequent element
+  }
+}
+mode_emissions <- mode(emissions); #Calculates the mode of emissions
+
+
+#Dispersion measures:
+
+amplitude_emissions <- diff(range(emissions));
+#Calculates the amplitude of emissions(difference between the largest and the smallest values)
+
+variance_emissions <- var(emissions); #Calculates the variance of emissions
+stDev_emissions <- sqrt(variance_emissions); #Calculates the standard deviation of emissions
+coefVar_emissions <- (stDev_emissions/mean_emissions) * 100; 
+#Calculates the coefficient of variation of emissions
+
+
+
+
